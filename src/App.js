8afc8289b1e1563002302959013d4e27ff7b46d5
khalidmansoor0310 <khalidmansoor725@gmail.com>
2022-02-22 04:09:42 -0800

    Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
+
 function App() {
-    
+   
   return (
-    <div>
-      this is simple app
-    </div>
+    <div></div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Store from './Store';
 import App from './App';
-import { Provider } from 'react-redux';
+
 
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={Store}>
             <App/>
-        </Provider>
     </React.StrictMode>
     
     ,
